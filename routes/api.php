6fd31972aca4e80841dccd4a4e74dfc04927f079
Mohammed El Habach <mhamadhabach06@gmail.com>
2023-08-08 00:45:05 +0300

    User Auth Done

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('/register', [AuthController::class,'register']);
+    Route::post('/login',[AuthController::class,'login']);
+    Route::post('/logout',[AuthController::class,'logout'] );
+    Route::get('/profile',[AuthController::class,'profile'] );
+
+
 });
